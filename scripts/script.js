271ea5e7f4fb71e8804e4ff37e3359dbb87d2af8
Jen Saxena <jen@jensaxena.com>
2018-03-01 17:26:27 -0500

    Add change handler for waveform presets

--- scripts/script.js
+++ scripts/script.js
@@ -6,12 +6,15 @@ synth.audio = new AudioContext();
 synth.wave = synth.audio.createOscillator();
 synth.vol = synth.audio.createGain();
 
-// SET DEFAULTS
-// synth.wave.type = 'sine';
-synth.wave.type = 'square';
-// synth.wave.type = 'triangle';
-// synth.wave.type = 'sawtooth';
+// SET DEFAULT WAVEFORM
+synth.wave.type = 'sine';
 
+// HANDLE WAVEFORM SELECTION
+$(`input[name="wave"]`).on('change', function (e) {
+  synth.wave.type = e.target.value;
+})
+
+// DEFAULT VOLUME = SHHH
 synth.vol.gain.value = 0;
 
 // PLUG IN
@@ -21,7 +24,7 @@ synth.vol.connect(synth.audio.destination);
 // ROCK
 synth.wave.start();
 
-synth.play = function() {
+synth.play = function () {
   if (!synth.freq) {
     $('.key').addClass('insist');
   }
@@ -34,7 +37,7 @@ synth.play = function() {
   };
 };
 
-synth.note = function() {
+synth.note = function () {
   synth.wave.frequency.setValueAtTime(synth.freq, synth.audio.currentTime);
 
   synth.vol.gain.value = 1;
@@ -42,8 +45,8 @@ synth.note = function() {
 };
 
 // INIT
-synth.init = function() {
-  $(window).keydown(function(e) {
+synth.init = function () {
+  $(window).keydown(function (e) {
     synth.key = $(`div[data-key="${e.keyCode}"]`);
     synth.freq = synth.key.attr('data-freq');
     synth.play(synth.key, synth.freq);
